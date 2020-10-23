6f4bf10dc977ed08c3121dca89bdfba583fd56ca
Raul Argott Castro <170009@upslp.edu.mx>
2020-10-23 16:36:42 -0500

    re v1.1

--- re/170009_re.fs
+++ re/170009_re.fs
@@ -1,21 +1,31 @@
-﻿// Learn more about F# at http://fsharp.org
-
+﻿(*
+ *  Nombre:     Raul Andres Argott Castro
+ *  Matricula:  170009
+ *  Carrera:    ITI
+ *  Description:  Expresion regular
+ *
+ *  Written:       22/10/2020
+ *
+ *  % F#
+ *  % F# script
+*)
 open System
 open System.Text.RegularExpressions
 
 let mutable tokens = []
 let sourceCode = "int result = 1;".Split(' ')
 
+//recorrido de la cadena
 for word in sourceCode do
-    if List.exists ((=)word) ["str"; "int"; "bool"] then
+    if List.exists ((=)word) ["str"; "int"; "bool"] then //evalua el DATATYPE
         tokens <- List.append tokens [word]
-    elif Regex.IsMatch(word, "[A-Za-z]") then
+    elif Regex.IsMatch(word, "[A-Za-z]") then //EVALUA EL NOMBRE DE VARIABLE
         tokens <- List.append tokens [word]
-    elif List.exists ((=)word) ["*";"-";"/";"+";"%";"="] then
+    elif List.exists ((=)word) ["*";"-";"/";"+";"%";"="] then //EVALUA OPERADOR
         tokens <- List.append tokens [word]
-    elif Regex.IsMatch (word,"[0-9]") then
+    elif Regex.IsMatch (word,"[0-9]") then //EVALUA EL DATO
         if word.[word.Length - 1] = ';' then
-            tokens <- List.append tokens [word.[0..(word.Length-1)]]
+            tokens <- List.append tokens [word.[0..(word.Length-2)]]
             tokens <- List.append tokens [";"]
         else
             tokens <- List.append tokens [word]
@@ -23,6 +33,7 @@ for word in sourceCode do
 
 printfn "%A" tokens 
     
+//Evaluacion del nombre de una variable    
 let variablePROLOG(w : byref<string>) : bool= 
     if (Char.IsLetter(w.[0]) && Char.IsUpper(w.[0]) || w.[0] = '_') then 
         w <- w.[1..(w.Length-1)]
@@ -30,7 +41,3 @@ let variablePROLOG(w : byref<string>) : bool=
             w <- w.[1..(w.Length-1)]
     String.IsNullOrEmpty(w)    
             
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
---
+++ re/README.md
@@ -0,0 +1,15 @@
+# Expresion regular
+
+## Descripción
+
+Programa escrito en F# para la evaluacion de una cadena utilizando expresiones regulares y simulando un compilador.
+
+## Ejecucuón
+
+Para ejecutarlo hay dos opciones:
+1. Ejecutando el proyecto con run
+    Para esto se debe ingresar en la consola dentro de la carpeta Conjuntos la siguiente luinea:
+
+        dotnet run 170009_re.fs
+
+    En esta ejecucion se imprimiran los resultados de todas las funciones a la vez y en el orden en el que se encuentran escritas en el main
